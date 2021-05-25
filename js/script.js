b8ca7d077938cad149cf9d98f22f095daacdca95
InfinitydeGuzma <>
2021-05-25 03:46:15 +0000

    switch elseif to ifelse

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ function doMathClicked () {
 
   if (userAge >= 17) {
 		document.getElementById('answer').innerHTML = "You can see R-rated films/movies alone :)";
-	} else if (userAge >= 13) {
+	} if else (userAge >= 13) {
 		document.getElementById('answer').innerHTML = "You can see PG-13 films/movies alone :)";
-	} else if (userAge >= 5) {
+	} if else (userAge >= 5) {
 		document.getElementById('answer').innerHTML = "You can see G or PG films/movies alone :D";
 	} else {
 		document.getElementById('answer').innerHTML = "Uhhhh… you’re too young to watch most things.";
